a1a7bd120563bea0b120d83a843f3fb944d645e7
Andrea Galvan <gandrea@vt.edu>
2024-12-11 21:57:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -30,10 +30,10 @@ st.subheader("Nicholas Jurczyk, Brandt Sandman, Andrea Galvan")
 
 # Input fields for the user, As we cannot measure the Viscera (organ) weight, shell weight, nor shucked weight without killing the crab, we will remove these from the dataset
 sex = st.selectbox("Sex (Female=0, Male=1, Indeterminate=2)", options=[0, 1, 2])
-length = st.number_input("Length (mm)", min_value=0.0, step=0.1)
-diameter = st.number_input("Diameter (mm)", min_value=0.0, step=0.1)
-height = st.number_input("Height (mm)", min_value=0.0, step=0.1)
-whole_weight = st.number_input("Whole Weight (g)", min_value=0.0, step=0.1)
+length = st.number_input("Length (cm)", min_value=0.0, step=0.1)
+diameter = st.number_input("Diameter (cm)", min_value=0.0, step=0.1)
+height = st.number_input("Height (cm)", min_value=0.0, step=0.1)
+whole_weight = st.number_input("Whole Weight (ounces)", min_value=0.0, step=0.1)
 
 
 # predict the age
